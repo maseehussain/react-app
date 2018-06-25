f982144829a5d9c999c6b5693112dc741de6950b
Masee Hussain <hussainmasee@gmail.com>
2018-06-25 15:43:12 +0100

    bind and arrow function
    using .bind and arrow function for click event

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = newName => {
     this.setState({
       persons: [
-        { name: "Hussain", age: 36 },
+        { name: newName, age: 36 },
         { name: "Maiwand", age: 22 },
         { name: "Kadie", age: 32 }
       ]
@@ -26,7 +26,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React-App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("Hussain")}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -34,6 +36,7 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Masee")}
         >
           My hobbies: Football
         </Person>
