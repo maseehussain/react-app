8debe8d2780604174d3c47bbfd91b8a17991525b
Masee Hussain <hussainmasee@gmail.com>
2018-06-25 12:37:05 +0100

    components
    using arrow function to create a component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Person from "./Person/Person.js";
 
 import "./styles.css";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Masee Hussain</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Person />
     </div>
   );
 }
