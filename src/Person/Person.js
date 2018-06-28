5c6c9e7a3c025ac584681c042c3e8501855a9a44
Masee Hussain <hussainmasee@gmail.com>
2018-06-28 09:33:09 +0100

    added error message
    with ErrorBoundary

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -8,6 +8,12 @@ const person = props => {
       width: "450px"
     }
   };
+
+  const rnd = Math.random();
+
+  if (rnd > 0.7) {
+    throw new Error("Something went wrong");
+  }
   return (
     <div className="Person" style={style}>
       <p onClick={props.click}>
