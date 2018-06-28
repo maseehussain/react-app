ae8ac84399ad399a58b387184769b41106b042c1
Masee Hussain <hussainmasee@gmail.com>
2018-06-28 09:49:43 +0100

    organised folders
    moved files and created folder for components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Radium, { StyleRoot } from "radium";
-import Person from "./Person/Person";
+import Person from "../components/Persons/Person/Person";
 
 import "./App.css";
 
@@ -67,14 +67,13 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <ErrorBoundary key={person.id}>
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}
-                  changed={event => this.nameChangeHandler(event, person.id)}
-                />
-              </ErrorBoundary>
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                changed={event => this.nameChangeHandler(event, person.id)}
+                key={person.id}
+              />
             );
           })}
         </div>
